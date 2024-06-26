20dac33d65debd769ae0ed31ccef2f95d56216e1
Nursaadet <nursaadetozdemir@gmail.com>
2024-06-26 21:32:34 +0300

    up

--- nodejs/class-notes/05-OOP_1/oop1.js
+++ nodejs/class-notes/05-OOP_1/oop1.js
@@ -59,7 +59,7 @@ console.log( Car['colors'][1] )
 console.log( Car['details']['engineSize'] )
 console.log( Car['startEngine']() )
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //* "THIS" Keyword:
 
 const Car = {
@@ -77,12 +77,48 @@ const Car = {
     startEngine: function(param = '1') {
         console.log(param)
         return 'Engine started.'
+    },
+    getDetails: function() {
+        console.log( this )
+        // return this.details
+        // return this.startEngine()
+        return this.brand + ' ' + this.model + ' ' + this.year + ' ' + this.startEngine()
+    },
+    arrowMethod: () => {
+        //* Arrow function is globalScope. (Not working this keyword in here)
+        console.log( this )
     }
 
 }
 
+console.log( Car.getDetails() )
+console.log( Car.arrowMethod() )
 
 /* ------------------------------------------------------- */
+//* ARRAY DESTRUCTURING:
+
+const testArray = ['value0', 'value1', 'value2', 'value3', 'value4']
+
+// const test0 = testArray[0]
+// const test1 = testArray[1]
+// const test2 = testArray[2]
+// // const test3 = testArray[3]
+// // const test4 = testArray[4]
+// const testPart = testArray.slice(3)
+// console.log(test0, test1, test2, testPart)
+
+// Sıralama Önemli:
+// const [var0, var1, ...varPart] = testArray
+// console.log(var0, var1, varPart)
+
+// REST Operator (Toplayıcı) (Eşitir ifadesinin sol tarafındaki REST operatördür.)
+const [var0, var1, ...varPart] = testArray
+console.log(var0, var1, varPart)
+
+// SPREAD Operator (Dağıtıcı/Serpiştirici) (Eşitir ifadesinin sağ tarafındaki SPREAD operatördür.)
+const newArr = [ ...testArray, 'value5', 'value6' ]
+console.log( newArr)
+
 /* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
