ebe0c74439aac5f0096ce409e80590b0dce96bb9
Nursaadet <nursaadetozdemir@gmail.com>
2024-06-26 22:35:57 +0300

    last

--- nodejs/class-notes/05-OOP_1/oop1.js
+++ nodejs/class-notes/05-OOP_1/oop1.js
@@ -94,7 +94,7 @@ const Car = {
 console.log( Car.getDetails() )
 console.log( Car.arrowMethod() )
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //* ARRAY DESTRUCTURING:
 
 const testArray = ['value0', 'value1', 'value2', 'value3', 'value4']
@@ -119,6 +119,94 @@ console.log(var0, var1, varPart)
 const newArr = [ ...testArray, 'value5', 'value6' ]
 console.log( newArr)
 
+/* ------------------------------------------------------- *
+//* OBJECT DESTRUCTURING:
+
+const Car = {
+
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1967,
+    isAutoGear: true,
+    colors: [ 'red', 'white' ],
+    details: {
+        color1: 'red',
+        color2: 'white',
+        engineSize: 5000
+    },
+    startEngine: function(param = '1') {
+        console.log(param)
+        return 'Engine started.'
+    }
+
+}
+console.log(typeof Car, Car)
+
+// REST Operator (KEY isimleri önemli!)
+// const { brand, year, model, ...others } = Car
+// console.log(brand, year, model, others)
+
+// İsim değiştirme:
+// const { brand, year, model: newName, ...others } = Car
+// console.log(brand, year, newName, others)
+
+// SPREAD Operator:
+// const newObj = { ...Car, newKey: 'newValue' }
+// console.log(newObj)
+
 /* ------------------------------------------------------- */
+
+// // Object to JSON:
+// console.log(typeof Car, Car)
+// const json = JSON.stringify(Car)
+// console.log(typeof json, json)
+
+// // JSON to Object:
+// const obj = JSON.parse(json)
+// console.log(obj)
+
+// Array to JSON:
+// const arr = Object.keys(Car)
+// // const arr = Object.values(Car)
+// // const arr = Object.entries(Car) // enumarateType
+// console.log(arr)
+// const arrToJSON = JSON.stringify(arr)
+// console.log(arrToJSON)
+
+// const jsonToArr = JSON.parse(arrToJSON)
+// console.log(typeof jsonToArr, jsonToArr)
+
+// console.log(typeof true, true)
+// console.log(typeof 1234, 1234)
+// console.log(typeof 'string', 'string')
+// console.log(typeof null, null)
+// console.log(typeof undefined, undefined)
+// console.log(typeof [], [])
+// console.log(typeof {}, {})
+// console.log(typeof function() {}, function() {})
+// const test = String('new-value')
+// console.log(typeof test, test)
+
+/* ------------------------------------------------------- *
+// CONTRUCTOR FUNCTIONS:
+
+const contructorFunction = function () {
+    this.property = 'value'
+}
+
 /* ------------------------------------------------------- */
+// 'NEW' Keyword:
+
+const carConstructor = function (brand, model, year = 2000) {
+
+    this.brand = brand
+    this.model = model
+    this.year = year
+
+    this.startEngine = function () {
+        return 'Engine started.'
+    }
+
+}
+
 /* ------------------------------------------------------- */
