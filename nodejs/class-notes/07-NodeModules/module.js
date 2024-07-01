23f265d3f9e78d5d96a1cd5dc3b16da45b2b1ae9
Nursaadet <nursaadetozdemir@gmail.com>
2024-07-01 23:50:07 +0300

    up

--- nodejs/class-notes/07-NodeModules/module.js
+++ nodejs/class-notes/07-NodeModules/module.js
@@ -14,6 +14,34 @@
 // const test = require('./modules/')
 // test()
 
+// require('./modules/')()
+
+/* -------------------------------------------- */
+
+// const arrFunc = require('./modules/')
+// // console.log(arrFunc)
+// arrFunc[0]()
+// arrFunc[1]()
+// arrFunc[2]()
+
+// Array Destructuring:
+// const [test1, test2, test3] = require('./modules/')
+// test1()
+// test2()
+// test3()
+
+// const objFunc = require('./modules/')
+// objFunc.test1()
+// objFunc.test2()
+// objFunc.test3()
+
+// Object Desctructuring:
+const { test1, test3:func3, test2, newVar } = require('./modules/')
+test1()
+test2()
+func3()
+console.log(newVar)
+
 
 /* -------------------------------------------- */
 /* -------------------------------------------- */
