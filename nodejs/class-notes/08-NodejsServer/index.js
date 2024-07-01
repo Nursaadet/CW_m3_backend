772860616e3cdf7a8b14e6738d273f8a8503a3b6
Nursaadet <nursaadetozdemir@gmail.com>
2024-07-02 00:00:27 +0300

    up

--- nodejs/class-notes/08-NodejsServer/index.js
+++ nodejs/class-notes/08-NodejsServer/index.js
@@ -10,3 +10,10 @@
 const http = require('node:http') // BuiltIn Module.
 
 /* -------------------------------------- */
+// http.createServer( (request, response) => { ... } )
+const app = http.createServer((request, response) => {
+
+        response.end('Hello World')
+        console.log('Console Print')
+    
+    })
