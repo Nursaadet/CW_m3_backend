df0f8a679678e7fad6451387bdef6a283f6082ed
Nursaadet <nursaadetozdemir@gmail.com>
2024-07-02 23:52:13 +0300

    up

--- nodejs/class-notes/08-NodejsServer/index.js
+++ nodejs/class-notes/08-NodejsServer/index.js
@@ -11,9 +11,15 @@ const http = require('node:http') // BuiltIn Module.
 
 /* -------------------------------------- */
 // http.createServer( (request, response) => { ... } )
+
 const app = http.createServer((request, response) => {
 
         response.end('Hello World')
         console.log('Console Print')
-    
-    })
+
+})
+    // Default server domain (local domain) = localhost
+// Default server IP (local IP) = 127.0.0.1
+app.listen(8000, () => console.log('Server started: http://127.0.0.1:8000'))
+
+/* -------------------------------------- *
