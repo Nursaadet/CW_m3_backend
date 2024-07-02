f39b7c70fc76300471ab7ccf2b2ec61ceb9462b2
Nursaadet <nursaadetozdemir@gmail.com>
2024-07-02 23:52:30 +0300

    up

--- nodejs/class-notes/08-NodejsServer/index.js
+++ nodejs/class-notes/08-NodejsServer/index.js
@@ -23,3 +23,28 @@ const app = http.createServer((request, response) => {
 app.listen(8000, () => console.log('Server started: http://127.0.0.1:8000'))
 
 /* -------------------------------------- *
+
+const app = http.createServer((req, res) => {
+
+    // console.log(req)
+    // console.log(res)
+    // console.log(req.url)
+
+    if (req.url == '/') {
+
+        res.end('Main Page')
+
+    } else if (req.url == '/second') {
+
+        res.end('second page')
+
+    } else {
+
+        res.end('any page')
+    }
+
+})
+
+app.listen(8000, () => console.log('http://127.0.0.1:8000'))
+
+/* -------------------------------------- */
