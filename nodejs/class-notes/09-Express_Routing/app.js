5455f1574614b3c2f2a0f84136ad08078e434987
Nursaadet <nursaadetozdemir@gmail.com>
2024-07-03 20:56:22 +0300

    up

--- nodejs/class-notes/09-Express_Routing/app.js
+++ nodejs/class-notes/09-Express_Routing/app.js
@@ -12,13 +12,9 @@ const app = express()
 
 // dotenv çalıştır:
 require('dotenv').config()
-
-
-
 const PORT = process.env.PORT || 8000
-app.listen()
 
-
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
 
 /* ----------------------------------------------- */
 /* ----------------------------------------------- */
