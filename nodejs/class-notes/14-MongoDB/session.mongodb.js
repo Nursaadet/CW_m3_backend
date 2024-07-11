cd19055fd6df49d32b9d40f576869333030b3a1e
Nursaadet <nursaadetozdemir@gmail.com>
2024-07-11 23:44:32 +0300

    mongodb.js

---
+++ nodejs/class-notes/14-MongoDB/session.mongodb.js
@@ -0,0 +1,6 @@
+	/* MongoSH */
+	// https://www.mongodb.com/developer/products/mongodb/cheat-sheet/
+	// https://www.mongodb.com/docs/manual/reference/sql-comparison/
+	// https://www.w3schools.com/mongodb/index.php
+
+	
